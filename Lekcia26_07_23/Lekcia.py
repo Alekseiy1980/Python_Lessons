a7877c32b185be80c8246b06e0ce8d8205f3cc5a
Alekseiy1980 <129889765+Alekseiy1980@users.noreply.github.com>
2023-07-26 20:00:16 +0300

    Функции

---
+++ Lekcia26_07_23/Lekcia.py
@@ -0,0 +1,24 @@
+# # def sum_numders(n, y ="Hello World"):
+# #     print(y)
+# #     summa = 0
+# #     for i in range (1, n+1):
+# #         summa += i
+# #     return (summa)
+# #
+# #
+# # summa = sum_numders(5, "qwerty")
+# # print(summa)
+#
+# def sum_str(*args):
+#     res = ''
+#     for i in args:
+#         res += i
+#         return  res
+#
+#
+# print(sum_str('q', 'e', 'l'))
+
+
+import modul1 as m1
+
+print(m1.max1(51,9))
