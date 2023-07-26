1acad1c818c709d93d4dbd96a1484544db2d15c7
Alekseiy1980 <129889765+Alekseiy1980@users.noreply.github.com>
2023-07-26 20:23:47 +0300

    Рекурсия

--- Lekcia26_07_23/Lekcia.py
+++ Lekcia26_07_23/Lekcia.py
@@ -35,3 +35,17 @@
 #
 # from modul1 import*
 # print(max1(7,134))
+
+'''
+Рекурсия
+'''
+
+def fib(n):
+    if n in[1,2]:
+        return 1
+    return fib(n - 1) + fib(n - 2)
+
+list_1 = []
+for i in range(1,10):
+    list_1.append(fib(i))
+print(list_1)
