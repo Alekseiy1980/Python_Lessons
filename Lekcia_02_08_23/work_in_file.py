0109756aa5d2360611a5f4a15f3c85c8aca92d1d
Алексей <129889765+Alekseiy1980@users.noreply.github.com>
2023-08-02 22:41:17 +0300

    Работа с файлами, lambda, map, filter

--- Lekcia_02_08_23/work_in_file.py
+++ Lekcia_02_08_23/work_in_file.py
@@ -1,25 +1,28 @@
-# # colors = ['red', '12345', 'blue']
-# # data = open('file.md', 'a')  # здесь указываем режим , в котором будем работать file - название файла , после точки указываем расширение .txt , .md как примеры
-# # data.writelines(colors)  # разделителей не будет
-# # data.close()
+# text = input("Введите текст -> ")
+# data = open('file.md', 'a')  # здесь указываем режим , в котором будем работать file - название файла , после точки указываем расширение .txt , .md как примеры
+# data.writelines(text)  # разделителей не будет
+# data.close()
+# #
 #
+# # '''
+# # идет перезапись файла и когда функция видит print она закрывает файл и выходит с функции перезаписи
+# # '''
+# text = input("Введите текст -> ")
 #
-# '''
-# идет перезапись файла и когда функция видит print она закрывает файл и выходит с функции перезаписи
-# '''
 # with open('file.md', 'w')as data:
 #     data.write('Line 1\n')
 #     data.write('Line 2\n')
+#     data.write( text )
 #
 # print(56)
-
-'''
-режим чтения
-
-'''
-
-path = 'file.md'
-data = open(path, 'r')
-for line in data:
-    print(line)
-data.close()
+#
+# '''
+# режим чтения
+#
+# '''
+#
+# path = 'file.md'
+# data = open(path, 'r')
+# for line in data:
+#     print(line)
+# data.close()
