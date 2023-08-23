079be3cf33a4fb7394f62aedc918196d51441774
Alekseiy1980 <129889765+Alekseiy1980@users.noreply.github.com>
2023-08-23 21:59:17 +0300

    Телефонная книга2

--- ClassWorck 22_08_23/main.py
+++ ClassWorck 22_08_23/main.py
@@ -21,29 +21,46 @@
 import json
 
 
-telephone_book = {"Василенко Алексей": {"telephone":[+79014823317, 342543254],"email":"Alekseiy80@mail.ru", "birth_day": "061080"},
-                  "Тетя Валя":{"telephone":[5642365432]}}
+telephone_books = []
 
 
-
-def save():
-    with open("telephone_book.json", "w", encoding="utf-8") as tb:
-        tb.write(json.dumps(telephone_book, ensure_ascii=False))
-    print("Телефонная книга успешно сохранена")
 
 def load():
     with open("telephone_book.json","r",encoding="utf-8") as tb:
-        telephone_book=json.load(tb)
-    print("Телефонная книга успешно загружена")
+        telephone_book = json.load(tb)
+    return  telephone_book
+
+def save():
+    with open("telephone_book.json", "w", encoding="utf-8") as tb:
+        tb.write(json.dumps(telephone_books, ensure_ascii=False))
+    print("Телефонная книга успешно сохранена")
 
 
 
 while True:
     command = input("Введите команду")
-    if command ==  "/contact":
+    if command ==  "/add":
         name = input("Введите имя нового контакта: ")
         number0 = input("Введите 1й номер: ")
         number1 = input("Введите 2й номер: ")
         birth_day = input("Введите ДР: ")
         mail = input("Введите email: ")
-        telephone_book[name] = {"telephone":[number0,number1],"email":mail, "birth_day": birth_day}
+        telephone_books[name] = {"telephone":[number0,number1],"email":mail, "birth_day": birth_day}
+    elif command == "/all":
+        print("Список контактов")
+        print(telephone_books)
+    elif command == "/find":
+        name = input("Введите имя для поиска: ")
+        if name in telephone_books:
+            print(telephone_books[name])
+    elif command == "/save":
+        save()
+    elif command == "/load":
+
+        telephone_books = load()
+        print ("Файлы успешно загружены")
+    elif command == "/stop":
+        save()
+        print("пока")
+        break
+
